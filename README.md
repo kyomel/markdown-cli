e7451cdfa1fedbd9f0476c4b69fb067640e57aa3
kyomel <michaellapandio04@gmail.com>
2025-05-02 21:37:01 +0700

    add preview html to browser

--- README.md
+++ README.md
@@ -6,11 +6,23 @@ This is an example Markdown file to test the preview tool
 
 - Support for links [Educative](https://www.educative.io/)
 - Support for other features
+- Direct README.md processing with `-readme` flag
 
 ## How to install:
 
-```
+```bash
 go get github.com/user/program
+
+# Build the binary
+go build -o mdp
+```
+
+## Testing
+
+To run the tests:
+
+```bash
+go test -v
 ```
 
 ## How to run:
@@ -24,8 +36,24 @@ go get github.com/user/program
    ```
    cd program
    ```
-4. Run the program:
-   ```
+4. Run the program using one of these methods:
+
+   ```bash
+   # Using go run
    go run main.go -file=your_markdown_file.md
+
+   # Using the built binary
+   ./mdp -file README.md
+
+   # Process README.md in current directory
+   go run main.go -readme
+   # or
+   ./mdp -readme
+
+   # Process README.md without preview
+   go run main.go -readme -s
+   # or
+   ./mdp -readme -s
    ```
-5. The HTML output will be generated in the same directory as your input file.
+
+5. The HTML output will be generated in a temporary directory and opened in your default browser (unless -s flag is used).
--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"io"
 	"os"
 
+	"os/exec"
+	"runtime"
+
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
 )
@@ -29,6 +32,7 @@ const (
 func main() {
 	// Parse flags
 	filename := flag.String("file", "", "Markdown file to preview")
+	skipPreview := flag.Bool("s", false, "Skip auto-preview")
 	flag.Parse()
 
 	// If user did not provide input file, show usage
@@ -37,13 +41,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename, os.Stdout); err != nil {
+	if err := run(*filename, os.Stdout, *skipPreview); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(filename string, out io.Writer) error {
+func run(filename string, out io.Writer, skipPreview bool) error {
 	// Read all the data from the input file and check for errors
 	input, err := os.ReadFile(filename)
 	if err != nil {
@@ -57,12 +61,23 @@ func run(filename string, out io.Writer) error {
 	if err != nil {
 		return err
 	}
-	defer temp.Close()
+	if err := temp.Close(); err != nil {
+		return err
+	}
 
 	outName := temp.Name()
+
 	fmt.Fprintln(out, outName)
 
-	return saveHTML(outName, htmlData)
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	return preview(outName)
 }
 
 func parseContent(input []byte) []byte {
@@ -85,4 +100,34 @@ func parseContent(input []byte) []byte {
 func saveHTML(outFname string, data []byte) error {
 	// Write the bytes to the file
 	return os.WriteFile(outFname, data, 0644)
+}
+
+func preview(fname string) error {
+	cName := ""
+	cParams := []string{}
+
+	// Define executable based on OS
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+		cParams = []string{"/C", "start"}
+	case "darwin":
+		cName = "open"
+	default:
+		return fmt.Errorf("OS not supported")
+	}
+
+	// Append filename to parameters slice
+	cParams = append(cParams, fname)
+
+	// Locate executable in PATH
+	cPath, err := exec.LookPath(cName)
+	if err != nil {
+		return err
+	}
+
+	// Open the file using default program
+	return exec.Command(cPath, cParams...).Run()
 }
